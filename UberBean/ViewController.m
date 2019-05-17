51e5d33f32151476f3ccb611205409b2c1a17fbe
Dayson Dong <daysond.yd@gmail.com>
2019-05-17 17:49:54 -0400

    first part done

--- UberBean/ViewController.m
+++ UberBean/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController ()
+
+@interface ViewController () <CLLocationManagerDelegate,MKMapViewDelegate>
+
+@property (nonatomic) CLLocationManager *locationManager;
+@property (weak, nonatomic) IBOutlet MKMapView *cafeMap;
 
 @end
 
@@ -16,7 +22,45 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self.cafeMap setShowsUserLocation:YES];
+    
+    [self.locationManager requestWhenInUseAuthorization];
+    
+}
+
+-(CLLocationManager*)locationManager {
+    
+    if (!_locationManager) {
+        _locationManager = [[CLLocationManager alloc]init];
+        _locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+        _locationManager.distanceFilter = 15;
+        _locationManager.delegate = self;
+    }
+    return _locationManager;
+}
+
+#pragma mark - Delegate
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
+    
+    CLLocation *location = locations[0];
+    MKCoordinateRegion region = MKCoordinateRegionMake(location.coordinate, MKCoordinateSpanMake(0.003, 0.003));
+    [self.cafeMap setRegion:region animated:YES];
+    
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    
+    NSLog(@"Error:%@",error);
+}
+
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+    NSLog(@"checking auth");
+    if (status == kCLAuthorizationStatusAuthorizedAlways || status == kCLAuthorizationStatusAuthorizedWhenInUse) {
+        [self.locationManager requestLocation];
+    }
+    
 }
 
 
